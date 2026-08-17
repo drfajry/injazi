781e097aaff956a68b4da5219bd6b37a4b8634d0
drfajry <dr.fajry@gmail.com>
2026-08-17 13:36:01 +0300

    Remove unused router import

--- injazi-flutter/lib/screens/home_screen.dart
+++ injazi-flutter/lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
-﻿import 'package:go_router/go_router.dart';
-import 'package:flutter/material.dart';
+﻿import 'package:flutter/material.dart';
 
 import '../models/evidence.dart';
 import '../widgets/coverage_card.dart';
